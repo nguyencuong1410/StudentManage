291f77a7d0543fd23a318386af47d0b94f8f196c
nguyencuonghou <89558925+nguyencuong1410@users.noreply.github.com>
2022-04-23 10:20:25 +0700

    update dapper

---
+++ StudentManage/Data/Dapper/ScoreDapper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Data.SqlClient;
+using Dapper;
+using StudentManage.Interface.Data;
+using StudentManage.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManage.Data.Dapper
+{
+    public class ScoreDapper : IScoreFileJson
+    {
+        private readonly string _conStr;
+        public ScoreDapper(string conStr)
+        {
+            _conStr = conStr;            
+        }
+
+        public void AddScore(Score score)
+        {
+            using (var connection = new SqlConnection(_conStr))
+            {
+                connection.Open();
+                //var affectedRow = connection.Execute("Insert into tblBangDiem (")
+            }
+        }
+
+        public List<Score> GetData()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
