6eab4171a9ba5c153ed64c5da2cbae65dfde90e3
nguyencuonghou <89558925+nguyencuong1410@users.noreply.github.com>
2022-04-29 10:30:24 +0700

    update Nhibernate

---
+++ StudentManage/Data/Hibernate/StudentHibernate.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NHibernate;
+using NHibernate.Cfg;
+using StudentManage.Interface.Data;
+using StudentManage.Models;
+
+namespace StudentManage.Data.Hibernate
+{
+    class StudentHibernate : IStudentData
+    {
+        public void AddStudent(Student stu)
+        {
+           using (ISession session = NHibernateSessions.OpenSession())
+            {
+                using(ITransaction transaction = session.BeginTransaction())
+                {
+                    session.Save(stu);
+                    transaction.Commit();
+                }
+            }
+        }
+
+        public List<Student> GetData()
+        {
+            List<Student> students = new List<Student>();
+            using (ISession session = NHibernateSessions.OpenSession())
+            {
+                students = session.Query<Student>().ToList();
+            }
+            return students;
+        }
+    }
+}
---
+++ StudentManage/Models/NHibernateSessions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NHibernate;
+using NHibernate.Cfg;
+using NHibernate.Driver;
+using NHibernate.Dialect;
+
+namespace StudentManage.Models
+{
+    class NHibernateSessions
+    {
+        public static ISession OpenSession()
+        {
+            var configuration = new Configuration();
+            configuration.Configure(@"D:\Source\C#\StudentManage\StudentManage\Models\hibernate.cfg.xml");
+            configuration.AddFile(@"D:\Source\C#\StudentManage\StudentManage\Mapping\Student.hbm.xml");
+            ISessionFactory sessionFactory = configuration.BuildSessionFactory();
+            return sessionFactory.OpenSession();
+        }
+    }
+}
